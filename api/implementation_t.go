271513efb030af9dd88759cd05fbbf442e4638b3
Jesse Williams <jesse@negativespacesounds.com>
2024-09-26 01:07:17 +1000

    consistent variables

--- api/implementation_t.go
+++ api/implementation_t.go
@@ -16,25 +16,25 @@ func NewTestApi(s *server.Server, l *slog.Logger) TestApi {
 	return TestApi{server: s, logger: l}
 }
 
-func (t TestApi) Server() *server.Server {
-	return t.server
+func (a TestApi) Server() *server.Server {
+	return a.server
 }
 
-func (t TestApi) UserAuthorize(next http.HandlerFunc) http.HandlerFunc {
+func (a TestApi) UserAuthorize(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		t.logger.Info("Would authenticate a reqest here")
+		a.logger.Info("Would authenticate a reqest here")
 		next(w, r)
 	}
 }
 
-func (t TestApi) UserRegister(w http.ResponseWriter, r *http.Request) {
-	t.logger.Info("would register a user here")
+func (a TestApi) UserRegister(w http.ResponseWriter, r *http.Request) {
+	a.logger.Info("would register a user here")
 }
 
-func (t TestApi) UserLogin(w http.ResponseWriter, r *http.Request) {
-	t.logger.Info("would log a user in here")
+func (a TestApi) UserLogin(w http.ResponseWriter, r *http.Request) {
+	a.logger.Info("would log a user in here")
 }
 
-func (t TestApi) ImageGenerate(w http.ResponseWriter, r *http.Request) {
-	t.logger.Info("would generate an image here")
+func (a TestApi) ImageGenerate(w http.ResponseWriter, r *http.Request) {
+	a.logger.Info("would generate an image here")
 }
